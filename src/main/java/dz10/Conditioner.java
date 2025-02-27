483ba8cd8f7a832d9d5b6c181d80f071848502e7
sergey.gavriliv <serez.gavriliv@mail.ru>
2025-02-27 14:00:31 +0300

    inner3(10)

--- src/main/java/dz10/Conditioner.java
+++ src/main/java/dz10/Conditioner.java
@@ -1,7 +1,7 @@
 package dz10;
 
 public class Conditioner {
-    public int currentTemperature;
+    private int currentTemperature;
 
     public int getCurrentTemperature() {
         return currentTemperature;
@@ -12,12 +12,27 @@ public class Conditioner {
             return;
         }
         if (newCurrentTemperature >= 30){
-            return;
+           newCurrentTemperature = 30;
         }
         currentTemperature = newCurrentTemperature;
     }
 
+    public void increaseTemp50p(){
+        int target = currentTemperature * 3 / 2;
+        setCurrentTemperature(target);
+    }
+
+    public void minusTemp50p() {
+        int target = currentTemperature / 2;
+        setCurrentTemperature(target);
+    }
+
     public void setTuMaxTemp() {
         currentTemperature = 30;
+    }
+
+    public void up1g(){
+        int gradus = currentTemperature + 1;
+        setCurrentTemperature(gradus);
     }
 }
--- src/test/java/ConditionerTest10dz.java
+++ src/test/java/ConditionerTest10dz.java
@@ -7,7 +7,7 @@ public class ConditionerTest10dz {
     Conditioner con = new Conditioner();
 
     @Test
-    public void shouldSetTemperature(){
+    public void shouldSetTemperature() {
         con.setCurrentTemperature(15);
 
         int expected = 15;
@@ -28,12 +28,68 @@ public class ConditionerTest10dz {
     }
 
     @Test
-    public void shouldNotSetTemperatureAboveMax(){
+    public void shouldNotSetTemperatureAboveMax() {
         con.setCurrentTemperature(50);
 
-        int expected = 0;
+        int expected = 30;
         int actual = con.getCurrentTemperature();
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void Temp50p() {
+        con.setCurrentTemperature(25);
+        con.increaseTemp50p();
+
+        int expected = 30;
+        int actual = con.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minus50p() {
+        con.setCurrentTemperature(10);
+        con.minusTemp50p();
+
+        int expected = 5;
+        int actual = con.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void upTo1gradusNeg() {
+        con.setCurrentTemperature(30);
+        con.up1g();
+
+        int expected = 30;
+        int actual = con.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void upTo1gradus() {
+        con.setCurrentTemperature(20);
+        con.up1g();
+
+        int expected = 21;
+        int actual = con.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void upTo1gradus1() {
+        con.setCurrentTemperature(-4);
+        con.up1g();
+
+        int expected = 1;
+        int actual = con.getCurrentTemperature();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
